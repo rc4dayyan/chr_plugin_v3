ce9e77246579fd457b1b8e08e135ae9c152fb2c2
rc4dayyan <daeytea@gmail.com>
2020-07-21 12:13:21 +0700

    initial

---
+++ precache-manifest.557160ff8c956f8f655272fef05f086e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f15e3f8ab24bad4cdaa6ae0e108af7e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bbb6fa6223fa238bca3b",
+    "url": "/static/css/2.f85989ec.chunk.css"
+  },
+  {
+    "revision": "8d739b33479004eb282f",
+    "url": "/static/css/main.1b24e2d3.chunk.css"
+  },
+  {
+    "revision": "bbb6fa6223fa238bca3b",
+    "url": "/static/js/2.6fc126d4.chunk.js"
+  },
+  {
+    "revision": "d47fff5cf394a4b69132e3b549607246",
+    "url": "/static/js/2.6fc126d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d739b33479004eb282f",
+    "url": "/static/js/main.a91a443b.chunk.js"
+  },
+  {
+    "revision": "439510a3e309d12ad760",
+    "url": "/static/js/runtime-main.3c2cd4ba.js"
+  }
+]);
